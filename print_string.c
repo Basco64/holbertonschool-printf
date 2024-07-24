052891157396c9e2cf60eaab2804a2a1e295b362
Basco <martinez.xabi@laposte.net>
2024-07-24 12:15:47 +0000

    bad pull

--- print_string.c
+++ print_string.c
@@ -8,10 +8,13 @@ void print_string(va_list args, int *count)
 {
 /* args extrait le prochain argument de type char* de la liste args */
 /* et le stocke dans le pointeur str. */
-char *str = va_arg(args, char*);
-while (*str) /* tant que str != '\0' */
-{
-_putchar(*str++); /* print le char pointé par str puis passe au suivant */
-(*count)++; /* incrémente count à chaque caractère print par _putchar */
-}
+	char *str = va_arg(args, char*);
+	
+	if (str == NULL)
+		return;
+	while (*str) /* tant que str != '\0' */
+	{
+		_putchar(*str++); /* print le char pointé par str puis passe au suivant */
+		(*count)++; /* incrémente count à chaque caractère print par _putchar */
+	}
 }
